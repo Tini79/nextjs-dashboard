33a1f61192714fd01fdc0798301401008e8b4496
Tini79 <artinichan79@gmail.com>
2024-01-06 11:17:04 +0700

    initial init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import { Inter, Lusitana } from 'next/font/google'; // import Inter which will be the primary font
+export const inter = Inter({ subsets: ['latin'] }) // specify what subset you'd like to download
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' })
